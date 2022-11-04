af1c40a25412750d4be8db705d35244ad5b95afd
robertfoerster <foersteroberthh@gmail.com>
2022-11-04 09:29:38 +0100

    kaggle helper, vis test file

--- utils.py
+++ utils.py
@@ -1,20 +1,23 @@
 import numpy as np
+import torch
 
 class KaggleAgent:
-    def __init__(self, model, game, augment=False):
+    def __init__(self, model, game):
         self.model = model
         self.game = game
-        self.augment = augment
 
     def run(self, obs, conf):
         player = obs['mark'] if obs['mark'] == 1 else -1
-        state = np.array(obs['board']).reshape(self.game.row_count, self.game.column_count)
-        state[state==2] = -1
-        
-        state = self.game.get_canonical_state(state, player)
-        valid_moves = self.game.get_valid_locations(state)
+        observation = np.array(obs['board']).reshape(self.game.row_count, self.game.column_count)
+        observation[observation==2] = -1
+        valid_moves = self.game.get_valid_locations(observation)
 
-        policy, value = self.model.predict(state, augment=self.augment)
+        encoded_observation = self.game.get_encoded_observation(observation)
+        canonical_observation = self.game.get_canonical_observation(encoded_observation, player)
+        hidden_state = torch.from_numpy(canonical_observation).unsqueeze(0)
+
+        policy, value = self.model.predict(hidden_state)
+        policy = policy.detach().cpu().numpy()[0]
         policy = policy * valid_moves
         action = int(np.argmax(policy))
         return action
