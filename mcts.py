softmax, fixed kaggle viz

mcts.py
@@ -90,7 +90,7 @@ class MCTS:
 
             canonical_hidden_state = self.game.get_canonical_state(node.state, node.player)
             action_probs, value = self.muZero.predict(canonical_hidden_state)
-            action_probs = action_probs[0]
+            action_probs = torch.softmax(action_probs, dim=1).squeeze(0)
             value = value.item()
 
             node.expand(action_probs)

utils.py
@@ -10,14 +10,18 @@ class KaggleAgent:
         player = obs['mark'] if obs['mark'] == 1 else -1
         observation = np.array(obs['board']).reshape(self.game.row_count, self.game.column_count)
         observation[observation==2] = -1
+        observation = torch.tensor(observation, dtype=torch.int8, device=self.game.device)
         valid_moves = self.game.get_valid_locations(observation)
+        print(valid_moves)
 
         encoded_observation = self.game.get_encoded_observation(observation)
-        canonical_observation = self.game.get_canonical_observation(encoded_observation, player)
-        hidden_state = torch.from_numpy(canonical_observation).unsqueeze(0)
+        canonical_observation = self.game.get_canonical_state(encoded_observation, player)
 
-        policy, value = self.model.predict(hidden_state)
-        policy = policy.detach().cpu().numpy()[0]
-        policy = policy * valid_moves
-        action = int(np.argmax(policy))
+        with torch.no_grad():
+            policy, _ = self.model.predict(canonical_observation)
+            policy = torch.softmax(policy, dim=1).squeeze(0)
+            policy = policy * valid_moves
+        print(policy)
+        action = torch.argmax(policy).item()
+        print(action)
         return action