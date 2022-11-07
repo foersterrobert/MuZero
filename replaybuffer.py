71fec16846aedc5bf293a5ef55f5dcb9be434991
robert foerster <foersteroberthh@gmail.com>
2022-11-07 20:03:50 +0100

    build trajectories

--- replaybuffer.py
+++ replaybuffer.py
@@ -2,41 +2,42 @@ import numpy as np
 
 class ReplayBuffer:
     def __init__(self, args, game):
-        self.buffer = []
+        self.memory = []
+        self.trajectories = []
         self.args = args
         self.game = game
 
+    def __len__(self):
+        return len(self.memory)
+
     def empty(self):
-        self.buffer = []
+        self.memory = []
 
     def add(self, game_memory):
-        self.buffer.extend(game_memory)
-
-    def sample(self, batch_size):
-        indices = np.random.choice(len(self.buffer), batch_size, replace=False)
-        trajectories = []
-        for i in indices:
-            observation, action, action_probs, final_reward, reward, game_idx, is_terminal = self.buffer[i]
+        self.memory.extend(game_memory)
+    
+    def build_trajectories(self):
+        self.trajectories = []
+        for i in range(len(self.memory)):
+            observation, action, policy, value, reward, game_idx, is_terminal = self.memory[i]
             if is_terminal:
                 action = np.random.choice(self.game.action_size)
-            action_probs_list, action_list, final_reward_list, reward_list = [action_probs], [action], [final_reward], [reward]
+            policy_list, action_list, value_list, reward_list = [policy], [action], [value], [reward]
 
             for k in range(1, self.args['K'] + 1):
-                if i + k < len(self.buffer) and self.buffer[i + k][5] == game_idx:
-                    _, next_action, next_action_probs, next_final_reward, next_reward, _, next_terminal = self.buffer[i + k]
+                if i + k < len(self.memory) and self.memory[i + k][5] == game_idx:
+                    _, next_action, next_policy, next_value, next_reward, _, next_terminal = self.memory[i + k]
                     if next_terminal:
                         next_action = np.random.choice(self.game.action_size)
                     action_list.append(next_action)
-                    action_probs_list.append(next_action_probs)
-                    final_reward_list.append(next_final_reward)
+                    policy_list.append(next_policy)
+                    value_list.append(next_value)
                     reward_list.append(next_reward)
 
                 else:
                     action_list.append(np.random.choice(self.game.action_size))
-                    action_probs_list.append(action_probs_list[-1])
-                    final_reward_list.append(-1 * final_reward_list[-1])
+                    policy_list.append(policy_list[-1])
+                    value_list.append(-1 * value_list[-1])
                     reward_list.append(-1 * reward_list[-1])
 
-            trajectories.append((observation, action_list, action_probs_list, final_reward_list, reward_list))
-        
-        return trajectories                        
+            self.trajectories.append((observation, action_list, policy_list, value_list, reward_list))
