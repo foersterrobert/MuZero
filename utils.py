2fa5bb4491162752c81a4c02ddf35be32da0e5ed
robert foerster <foersteroberthh@gmail.com>
2022-11-11 17:28:02 +0100

    utils np

--- utils.py
+++ utils.py
@@ -10,15 +10,15 @@ class KaggleAgent:
         player = obs['mark'] if obs['mark'] == 1 else -1
         observation = np.array(obs['board']).reshape(self.game.row_count, self.game.column_count)
         observation[observation==2] = -1
-        observation = torch.tensor(observation, dtype=torch.int8, device=self.game.device)
         valid_moves = self.game.get_valid_locations(observation)
         
         encoded_observation = self.game.get_encoded_observation(observation)
-        canonical_observation = self.game.get_canonical_state(encoded_observation, player)
+        canonical_observation = self.game.get_canonical_state(encoded_observation, player).copy()
 
         with torch.no_grad():
-            policy, _ = self.model.predict(canonical_observation)
-            policy = torch.softmax(policy, dim=1).squeeze(0)
+            canonical_observation = torch.tensor(canonical_observation, dtype=torch.float32, device=self.model.device)
+            policy, _ = self.model.predict(canonical_observation.unsqueeze(0))
+            policy = torch.softmax(policy, dim=1).squeeze(0).cpu().numpy()
             policy = policy * valid_moves
-        action = torch.argmax(policy).item()
+        action = np.argmax(policy)
         return action
