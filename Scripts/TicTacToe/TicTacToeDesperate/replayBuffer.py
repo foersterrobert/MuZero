8aca156cb5039858f9fa3a35f5a0a280ac70bebc
robertfoerster <foersteroberthh@gmail.com>
2023-06-01 14:55:06 +0200

    is_terminal epiphany

--- Scripts/TicTacToe/TicTacToeDesperate/replayBuffer.py
+++ Scripts/TicTacToe/TicTacToeDesperate/replayBuffer.py
@@ -16,23 +16,21 @@ class ReplayBuffer:
 
     def build_trajectories(self):
         for i in range(len(self.memory)):
-            observation, action, policy, value, game_idx, is_terminal = self.memory[i]
-            if not is_terminal:
-                policy_list, action_list, value_list = [policy], [action], [value]
+            observation, action, policy, value, game_idx = self.memory[i]
+            policy_list, action_list, value_list = [policy], [action], [value]
 
-                for k in range(1, self.args['K'] + 1):
-                    if i + k < len(self.memory) and self.memory[i + k][4] == game_idx:
-                        _, action, policy, value, _, is_terminal = self.memory[i + k]
-                        if is_terminal:
-                            action = np.random.choice(self.game.action_size)
-                        policy_list.append(policy)
-                        action_list.append(action)
-                        value_list.append(value)
+            for k in range(1, self.args['K'] + 1):
+                if i + k < len(self.memory) and self.memory[i + k][4] == game_idx:
+                    _, action, policy, value, _ = self.memory[i + k]
+                    policy_list.append(policy)
+                    action_list.append(action)
+                    value_list.append(value)
 
-                    else:
-                        policy_list.append(np.ones(self.game.action_size) / self.game.action_size)
-                        action_list.append(np.random.choice(self.game.action_size))
-                        value_list.append(self.game.get_opponent_value(value_list[-1]))
+                else:
+                    policy_list.append(np.ones(self.game.action_size) / self.game.action_size)
+                    action_list.append(np.random.choice(self.game.action_size))
+                    value_list.append(0)
+                    # value_list.append(self.game.get_opponent_value(value_list[-1]))
 
-                policy_list = np.stack(policy_list)
-                self.trajectories.append((observation, policy_list, action_list, value_list))
+            policy_list = np.stack(policy_list)
+            self.trajectories.append((observation, policy_list, action_list, value_list))
